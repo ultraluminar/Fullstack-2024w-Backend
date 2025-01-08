5bca5f560a7de160172262cbc2e94bd1e58883d3
Adrian Lösch <loeschadrianp+git@gmail.com>
2025-01-08 17:48:34 +0100

    feat: add express and typeorm

---
+++ src/routes/usersRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const usersRouter = Router();
+
+// usersRouter.post('/login', authController.login);
+// usersRouter.post('/register', authController.register);
+// usersRouter.delete('/:userId', usersController.deleteUser);
+// usersRouter.get('/:userId', usersController.getUserById);
+
+export { usersRouter };
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-// TODO: Implement the server
+import "reflect-metadata";
+import express from 'express';
+import bodyParser from 'body-parser';
+import { AppDataSource } from "./data-source.js";
+import { usersRouter } from './routes/usersRouter.js';
+
+await AppDataSource.initialize();
+console.log("AppDataSource initialized successfully");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/users', usersRouter);
+
+const { BACKEND_PORT_INTERNAL, BACKEND_PORT } = process.env;
+
+app.listen(BACKEND_PORT_INTERNAL, () => {
+  console.log(`Server is listening on http://localhost:${BACKEND_PORT}`);
+});
