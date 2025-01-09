f17cd70420e0ac604b220c7f15144abca50dea2d
Lukas Klassen <inf4143@hs-worms.de>
2025-01-09 15:22:25 +0100

    feat: add router and empty controller methods

---
+++ src/controllers/questionsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+export const questionsController = {
+    async getQuestions(request: Request, response: Response) {
+        // TODO: implement
+        response.status(501).end();
+    },
+    async createQuestion(request: Request, response: Response) {
+        // TODO: implement
+        response.status(501).end();
+    },
+    async getQuestionById(request: Request, response: Response) {
+        // TODO: implement
+        response.status(501).end();
+    },
+    async updateQuestion(request: Request, response: Response) {
+        // TODO: implement
+        response.status(501).end();
+    },
+    async deleteQuestion(request: Request, response: Response) {
+        // TODO: implement
+        response.status(501).end();
+    },
+};
