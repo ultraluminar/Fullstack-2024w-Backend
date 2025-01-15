6034ca9214c26aa7a4e0c0a4dfb08428b41ba385
Lukas Klassen <inf4143@hs-worms.de>
2025-01-15 20:42:31 +0100

    feat: implement deleteAnswer method

--- src/controllers/answersController.ts
+++ src/controllers/answersController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { ErrorResponse } from "../models/ErrorResponse.js";
 import { Answer } from "../models/answer/Answer.js";
 import { PublicAnswer } from "../models/answer/PublicAnswer.js";
+import { Token } from "../models/Token.js";
+import { User } from "../models/user/User.js";
 
 export const answersController = {
     async getAnswerById(request: Request, response: Response) {
@@ -28,7 +30,41 @@ export const answersController = {
         response.sendStatus(501);
     },
     async deleteAnswer(request: Request, response: Response) {
-        //TODO: implement
-        response.sendStatus(501);
+        const token = Token.fromRequest(request);
+        if (token == null) {
+            const errorResponse = ErrorResponse.invalidToken();
+            response.status(401).json(errorResponse);
+            return;
+        }
+        const userId = token.userId;
+        const user = await User.findOneBy({ id: userId });
+        if (user == null) {
+            const errorResponse = ErrorResponse.userNotFound(userId);
+            response.status(404).json(errorResponse);
+            return;
+        }
+        const answerId = Number(request.params.answerId);
+        if (isNaN(answerId)){
+            const errorResponse = ErrorResponse.invalidId(answerId);
+            response.status(400).json(errorResponse);
+            return;
+        }
+        const answer = await Answer.findOne({
+            where: { id: answerId },
+            relations: { user: true },
+        });
+        if (answer == null){
+            const errorResponse = ErrorResponse.questionNotFound(answerId);
+            response.status(404).json(errorResponse);
+            return;
+        }
+        if (!token.isAutherizedUser(answer.user)){
+            const errorResponse = ErrorResponse.forbiddenAction();
+            response.status(403).json(errorResponse);
+            return;
+        }
+        await answer.remove();
+        console.log(`Answer with ID "${answerId}" removed`);
+        response.status(204).end();
     },
 };
