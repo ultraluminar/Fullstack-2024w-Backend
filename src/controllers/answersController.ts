229d4dc2d46b98aa6f557a40954bd00b7807be80
Lukas Klassen <inf4143@hs-worms.de>
2025-01-15 20:39:11 +0100

    feat: implement getAnswerById method

--- src/controllers/answersController.ts
+++ src/controllers/answersController.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from "express";
+import { ErrorResponse } from "../models/ErrorResponse.js";
+import { Answer } from "../models/answer/Answer.js";
+import { PublicAnswer } from "../models/answer/PublicAnswer.js";
 
 export const answersController = {
     async getAnswerById(request: Request, response: Response) {
-        //TODO: implement
-        response.sendStatus(501);
+        const answerId = Number(request.params.answerId);
+        if (isNaN(answerId)){
+            const errorResponse = ErrorResponse.invalidId(answerId);
+            response.status(400).json(errorResponse);
+            return;
+        }
+        const answer = await Answer.findOne({
+            where: { id: answerId },
+            relations: { user: true },
+        });
+        if (answer == null){
+            const errorResponse = ErrorResponse.questionNotFound(answerId);
+            response.status(404).json(errorResponse);
+            return;
+        }
+        const publicAnswer = PublicAnswer.fromAnswer(answer);
+        response.status(201).json(publicAnswer);
     },
     async updateAnswer(request: Request, response: Response) {
         //TODO: implement
