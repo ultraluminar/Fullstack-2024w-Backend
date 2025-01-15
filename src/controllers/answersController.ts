b93098dbdbb209ce6d464038747e45587e7616e3
Lukas Klassen <inf4143@hs-worms.de>
2025-01-15 15:29:53 +0100

    feat: add empty answersController methods

---
+++ src/controllers/answersController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export const answersController = {
+    async getAnswerById(request: Request, response: Response) {
+        //TODO: implement
+        response.sendStatus(501);
+    },
+    async updateAnswer(request: Request, response: Response) {
+        //TODO: implement
+        response.sendStatus(501);
+    },
+    async deleteAnswer(request: Request, response: Response) {
+        //TODO: implement
+        response.sendStatus(501);
+    },
+};
