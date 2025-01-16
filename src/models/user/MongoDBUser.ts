87d798a8ed6bbe64f13aecd3ba9eed23d63d32bb
Lukas Klassen <inf4143@hs-worms.de>
2025-01-16 20:46:26 +0100

    feat: add MongoDBUser model

---
+++ src/models/user/MongoDBUser.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Column, Entity, ObjectId, ObjectIdColumn } from "typeorm";
+
+@Entity()
+export class MongoDBUser extends BaseEntity {
+
+    @ObjectIdColumn()
+    id: ObjectId;
+
+    @Column()
+    userId: number;
+
+    @Column()
+    loginCouter: number = 0;
+
+    @Column()
+    searchCounter: number = 0;
+
+    @Column()
+    questionsCreate: number = 0;
+
+    @Column()
+    questionsUpdated: number = 0;
+
+    @Column()
+    questionsDeleted: number = 0;
+
+    @Column()
+    answersCreated: number = 0;
+
+    @Column()
+    answersUpdated: number = 0;
+
+    @Column()
+    answersDeleted: number = 0;
+
+    static async findOrCreate(userId: number): Promise<MongoDBUser> {
+        return (await MongoDBUser.findOneBy({ userId: userId })) ?? MongoDBUser.create({ userId: userId });
+    }
+}
