6430b870094bd7195450f32a193975b28469fbf5
Lukas Klassen <inf4143@hs-worms.de>
2025-01-18 14:28:35 +0100

    feat: add isAdmin field to User and PublicUser

--- src/models/user/PublicUser.ts
+++ src/models/user/PublicUser.ts
@@ -3,16 +3,18 @@ import { User as PublicUserInterface } from "../../../../interface/user.js";
 
 export class PublicUser implements PublicUserInterface {
     id: number;
+    isAdmin: boolean;
     username: string;
     createdAt: Date;
 
-    constructor(id: number, username: string, created_at: Date) {
+    constructor(id: number, isAdmin: boolean, username: string, created_at: Date) {
         this.id = id;
+        this.isAdmin = isAdmin
         this.username = username;
         this.createdAt = created_at;
     }
 
     static fromUser(user: User): PublicUser {
-        return new PublicUser(user.id, user.username, user.created_at);
+        return new PublicUser(user.id, user.isAdmin, user.username, user.created_at);
     }
 }
