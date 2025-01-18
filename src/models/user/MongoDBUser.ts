1735ba9bccadd1c63c3abad47fbdd220d9274d77
Lukas Klassen <inf4143@hs-worms.de>
2025-01-18 13:32:50 +0100

    refactor: use user typeorm default values

--- src/models/user/MongoDBUser.ts
+++ src/models/user/MongoDBUser.ts
@@ -1,34 +1,33 @@
-import { BaseEntity, Column, Entity, ObjectId, ObjectIdColumn } from "typeorm";
+import { BaseEntity, Column, Entity, ObjectId, ObjectIdColumn, Repository } from "typeorm";
 
 @Entity()
 export class MongoDBUser extends BaseEntity {
-
     @ObjectIdColumn()
     id: ObjectId;
 
     @Column()
     userId: number;
 
-    @Column()
-    loginCouter: number = 0;
+    @Column({ default: 0 })
+    loginCouter: number;
 
-    @Column()
-    questionsCreated: number = 0;
+    @Column({ default: 0 })
+    questionsCreated: number;
 
-    @Column()
-    questionsUpdated: number = 0;
+    @Column({ default: 0 })
+    questionsUpdated: number;
 
-    @Column()
-    questionsDeleted: number = 0;
+    @Column({ default: 0 })
+    questionsDeleted: number;
 
-    @Column()
-    answersCreated: number = 0;
+    @Column({ default: 0 })
+    answersCreated: number;
 
-    @Column()
-    answersUpdated: number = 0;
+    @Column({ default: 0 })
+    answersUpdated: number;
 
-    @Column()
-    answersDeleted: number = 0;
+    @Column({ default: 0 })
+    answersDeleted: number;
 
     static async findOrCreate(userId: number): Promise<MongoDBUser> {
         return (await MongoDBUser.findOneBy({ userId: userId })) ?? MongoDBUser.create({ userId: userId });
